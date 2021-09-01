a9b502295bda9c0ab243713321bd508e33c70dc0
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-09-01 19:56:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,4 +75,27 @@ resource "aws_instance" "spoke1_ec2" {
     ami = "ami-00f22f6155d6d92c5"
     instance_type = "t2.micro"
     subnet_id = aviatrix_vpc.default[0].subnets[0].subnet_id
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    key_name = "KP_ESK_EC2_INST1"
 }
+
+#define AWS resources
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+} 
